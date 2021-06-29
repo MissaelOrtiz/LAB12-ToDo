2524130ea39a410058cb822fb8472c919d71c49f
Missael Ortiz <ortiz.missael80@gmail.com>
2021-06-29 16:21:08 -0700

    added working tests for get and post routes

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -28,23 +28,70 @@ describe('app routes', () => {
       return client.end(done);
     });
 
+    test('posts todos', async() => {
+
+      const expectation = [
+        {
+          'id': 4,
+          'todo': 'thing',
+          'completed': false,
+          'owner_id': 2
+        },
+        {
+          'id': 5,
+          'todo': 'thing2',
+          'completed': false,
+          'owner_id': 2
+        },
+        {
+          'id': 6,
+          'todo': 'thing3',
+          'completed': false,
+          'owner_id': 2
+        }
+      ];
+
+      for(let task of expectation) {
+        await fakeRequest(app)
+          .post('/api/todos')
+          .send(task)
+          .set('Authorization', token)
+          .expect('Content-Type', /json/)
+          .expect(200);
+      }
+
+      const data = await fakeRequest(app)
+        .get('/api/todos')
+        .set('Authorization', token)
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      expect(data.body).toEqual(expectation);
+    });
+
     test('gets todos for specific user', async() => {
 
       const expectation = [
         {
-          todo: 'thing',
-          completed: false
+          'id': 4,
+          'todo': 'thing',
+          'completed': false,
+          'owner_id': 2
         },
         {
-          todo: 'thing2',
-          completed: false
+          'id': 5,
+          'todo': 'thing2',
+          'completed': false,
+          'owner_id': 2
         },
         {
-          todo: 'thing3',
-          completed: false
+          'id': 6,
+          'todo': 'thing3',
+          'completed': false,
+          'owner_id': 2
         }
       ];
-      console.log(token);
+
       const data = await fakeRequest(app)
         .get('/api/todos')
         .set('Authorization', token)
