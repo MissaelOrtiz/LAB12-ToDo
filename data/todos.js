5000afb34eb6fcc2bbbc7702c7a812060f17c6ae
Missael Ortiz <ortiz.missael80@gmail.com>
2021-06-29 15:17:05 -0700

    added working db

---
+++ data/todos.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    todo: 'thing',
+    completed: false
+  },
+  {
+    todo: 'thing2',
+    completed: false
+  },
+  {
+    todo: 'thing3',
+    completed: false
+  }
+];
